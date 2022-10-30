c8370efc10c91326876831714ad5fa92a0cc20b7
darkrell <rc-alh@outlook.com>
2022-10-30 19:09:43 +0300

    Added Tracing extension

---
+++ Darkrell.Pro.Blazor/Extensions/BindExtentions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Darkrell.Pro.Blazor.Extensions;
+
+public static class BindExtentions
+{
+    public static BindWrapper<TProperty> Trace<TSource, TProperty>(this TSource source, Expression<Func<TSource, TProperty>> propertyLambda, Action<TProperty> OnChanged)
+    {
+        Type type = typeof(TSource);
+
+        MemberExpression member = propertyLambda.Body as MemberExpression;
+        if (member == null)
+            throw new ArgumentException(string.Format(
+                "Expression '{0}' refers to a method, not a property.",
+                propertyLambda.ToString()));
+
+        PropertyInfo propInfo = member.Member as PropertyInfo;
+        if (propInfo == null)
+            throw new ArgumentException(string.Format(
+                "Expression '{0}' refers to a field, not a property.",
+                propertyLambda.ToString()));
+
+        if (type != propInfo.ReflectedType &&
+            !type.IsSubclassOf(propInfo.ReflectedType))
+            throw new ArgumentException(string.Format(
+                "Expression '{0}' refers to a property that is not from type {1}.",
+                propertyLambda.ToString(),
+                type));
+        return new BindWrapper<TProperty>(() => (TProperty)propInfo.GetValue(source), v => { propInfo.SetValue(source, v); OnChanged(v); });
+    }
+}
---
+++ Darkrell.Pro.Blazor/Extensions/BindWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Darkrell.Pro.Blazor.Extensions;
+
+public class BindWrapper<T>
+{
+
+    public BindWrapper(Func<T> getter, Action<T> setter)
+    {
+        _getter = getter;
+        _setter = setter;
+    }
+
+    private Func<T> _getter;
+    private Action<T> _setter;
+    public T V { get => _getter(); set => _setter(value); }
+}
